5bd67d083d17318d7560378e8d7982160054429b
Vitaliy Dakhnovets <40395540+inwake@users.noreply.github.com>
2022-09-14 16:40:34 -0400

    Adding an index file to grant access to utilities

---
+++ index.js
@@ -0,0 +1,38 @@
+import ajaxRequest from './ajaxRequest.js';
+import binarySearch from './binarySearch.js';
+import checkMobileBrowser from './checkMobileBrowser.js';
+import compressImage from './compressImage.js';
+import daysAgoDate from './daysAgoDate.js';
+import extractNestedValue from './extractNestedValue.js';
+import formatMoney from './formatMoney.js';
+import formatNumberWithCommas from './formatNumberWithCommas.js';
+import formatUsPhoneNumber from './formatUsPhoneNumber.js';
+import generatePassword from './generatePassword.js';
+import hexColorIsLight from './hexColorIsLight.js';
+import imgUrlToBase64 from './imgUrlToBase64.js';
+import insertionSort from './insertionSort.js';
+import mergeSort from './mergeSort.js';
+import parseRailsErrors from './parseRailsErrors.js';
+import parseUrlParams from './parseUrlParams.js';
+import percentage from './percentage.js';
+import similarityOfStrings from './similarityOfStrings.js';
+
+
+export default {ajaxRequest,
+    binarySearch,
+    checkMobileBrowser,
+    compressImage,
+    daysAgoDate,
+    extractNestedValue,
+    formatMoney,
+    formatNumberWithCommas,
+    formatUsPhoneNumber,
+    generatePassword,
+    hexColorIsLight,
+    imgUrlToBase64,
+    insertionSort,
+    mergeSort,
+    parseRailsErrors,
+    parseUrlParams,
+    percentage,
+    similarityOfStrings};
