bbd3c0d14df75c3c7ad3d020c6bb9ad24907396c
Vitaliy <vitaliy@bybeapp.com>
2023-02-02 20:17:58 +0100

    Fixing separator in formatDate

--- formatDate.js
+++ formatDate.js
@@ -2,5 +2,5 @@ export default function formatDate(date, separator = '.') {
   const month = date.getMonth() + 1;
   const day = date.getDate();
   const year = date.getFullYear();
-  return `${month}.${day}.${year}`;
+  return `${month}${separator}${day}${separator}${year}`;
 }
