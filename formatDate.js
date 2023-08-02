c7b17c4b3fefcea44f24909b63cb17fa0ec3606e
Vitaliy <vitaliy@bybeapp.com>
2023-08-02 23:44:22 +0200

    Much better formatDate, interface unchanged

--- formatDate.js
+++ formatDate.js
@@ -1,6 +1,41 @@
-export default function formatDate(date, separator = '.') {
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const year = date.getFullYear();
-  return `${month}${separator}${day}${separator}${year}`;
+export default function formatDate(date,
+  separator = '.',
+  order = ['month', 'day', 'year']) {
+  const {validDate, validSeparator, validOrder} =
+    validArgs({date, separator, order})
+  const monthNumMap = [1,2,3,4,5,6,7,8,9,10,11,12]
+  const monthIndex = validDate.getMonth()
+  const month = monthNumMap[monthIndex]
+  const day = validDate.getDate()
+  const year = validDate.getFullYear()
+  const components = validOrder.reduce(
+    function(components, el) {
+      switch (el) {
+        case 'year':
+          components.push(year)
+          break
+        case 'month':
+          components.push(month)
+          break
+        case 'day':
+          components.push(day)
+          break
+        default:
+          break}
+      return components}, [])
+  return components
+    .join(validSeparator)
 }
+
+function validArgs({date, separator, order}) {
+  const validDate = new Date(date)
+  const validSeparator = String(separator)
+  if (validDate.toString() === 'Invalid Date') {
+    throw new Error('Invalid Date')}
+  if (!(Array.isArray(order) && order.length === 3))
+    throw new Error('The order does not match the required format')
+  const validOrder = order.map(function(el) {
+    return String(el).toLowerCase()})
+  return {validOrder,
+    validSeparator,
+    validDate}}
